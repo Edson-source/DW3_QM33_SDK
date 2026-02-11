40d50b754f466adb5457406b0a0e43fa55b323c1
Edson-source <edsonneyjr@gmail.com>
2026-02-11 14:35:13 -0300

    criação do sistema anti_colisão

---
+++ Projects/FreeRTOS/Anticollision/Common/include/anticollision.h
@@ -0,0 +1,17 @@
+/**
+ * @file      anticollision.h
+ *
+ * @brief     Interface for anticollision application
+ *
+ * @author    Qorvo Applications
+ *
+ * @copyright SPDX-FileCopyrightText: Copyright (c) 2024-2025 Qorvo US, Inc.
+ *            SPDX-License-Identifier: LicenseRef-QORVO-2
+ *
+ */
+
+#pragma once
+
+#include "deca_error.h"
+
+error_e anticollision_init(void);
